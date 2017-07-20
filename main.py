66ae3ec1918373f01ff4fcaffcf16a7575238c44
Ricardo Azpeitia Pimentel <ricardo.azpeitia@adroll.com>
2017-07-19 20:18:55 -0500

    Initial setup

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import pygame
+import os
+
+# Special code to center window
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+FRAMES_PER_SECOND = 30
+RESOLUTION = WIDTH, HEGIHT = 800, 600
+WINDOWS_TITLE = 'Space Invaders v1.0'
+
+
+def setup():
+    pygame.init()
+    screen = pygame.display.set_mode(RESOLUTION)
+    pygame.display.set_caption(WINDOWS_TITLE)
+    return screen, False, pygame.time.Clock()
+
+
 def main():
-    pass
+    # Configuracion inicial
+    screen, done, clock = setup()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # Espera hasta el siguiente frame
+        clock.tick(FRAMES_PER_SECOND)
+    pygame.quit()
 
 
 main()
