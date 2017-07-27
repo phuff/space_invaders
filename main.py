f243572ec2951a2d3400d5b41f87c1437923ff05
Ricardo Azpeitia Pimentel <ricardo.azpeitia@adroll.com>
2017-07-26 22:10:45 -0500

    Adding a player

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ import pygame
 from pygame.locals import *
 
 
+class Player(object):
+    HEGIHT = 20
+    WIDTH = 60
+    COLOR = (0, 255, 0)
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    @property
+    def size(self):
+        return (self.WIDTH, self.HEGIHT)
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        rect = pygame.Rect(self.pos, self.size)
+        pygame.draw.rect(screen, self.COLOR, rect)
+
+
 class Game(object):
 
     FRAMES_PER_SECOND = 30
@@ -18,6 +43,7 @@ class Game(object):
         self.screen = pygame.display.set_mode(self.RESOLUTION)
         self.clock = pygame.time.Clock()
         pygame.display.set_caption(self.WINDOWS_TITLE)
+        self.player = Player(self.WIDTH // 2 - Player.WIDTH // 2, self.HEGIHT - Player.HEGIHT)
 
     def update(self):
         # Handle exit events
@@ -33,6 +59,7 @@ class Game(object):
 
     def draw(self):
         self.screen.fill(self.BLACK)
+        self.player.draw(self.screen)
         pygame.display.flip()
 
     def wait(self):
